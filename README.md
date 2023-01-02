0446bd00cf2b8afb750f3aa0fcb0ce43687b9fa8
J1min <aoiharu41@gmail.com>
2023-01-02 20:15:26 +0900

    Init: next13 with yarn berry

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nextjs Template
+
+1. Nextjs + Yarn berry + TypeScript
+2. axios
+3. @emotion/styled
+4. @emotion/react
+5. emotion-reset
---
+++ lib/instance.ts
@@ -0,0 +1,19 @@
+import { env } from "../config";
+import axios, { AxiosError, AxiosInstance } from "axios";
+
+const instance: AxiosInstance = axios.create({
+  baseURL: "",
+  timeout: 10000,
+  headers: { "Content-Type": "application/json" },
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error: AxiosError) {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
